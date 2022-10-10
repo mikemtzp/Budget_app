1255e93fe4eaaf5129cdf7141a259cb5cf32691e
mikemtzp <mikemp8226@gmail.com>
2022-10-10 15:43:44 -0500

    Create expenses_groups join table and run migration

---
+++ db/migrate/20221010203425_create_groups_expenses_join_table.rb
@@ -0,0 +1,8 @@
+class CreateGroupsExpensesJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :groups, :expenses do |t|
+      t.index :group_id
+      t.index :expense_id
+    end
+  end
+end
