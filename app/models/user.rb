9257bcce807cdc437e93408b0777ee7c84772114
mikemtzp <mikemp8226@gmail.com>
2022-10-10 11:52:25 -0500

    Add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :expenses, dependent: :destroy
   has_many :groups, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
 end
