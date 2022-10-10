60c23bd0c3c8d9ae26ff67a13394c60262f6ff1a
mikemtzp <mikemp8226@gmail.com>
2022-10-10 15:33:41 -0500

    Change  group and expense model associations to many-to-many

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  has_many :groups, dependent: :destroy
+  has_and_belongs_to_many :groups
 
   validates :name, presence: true, length: { minimum: 1, maximum: 50 }
   validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
